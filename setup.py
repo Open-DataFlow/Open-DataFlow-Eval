6471481094f0fb2df02634ef321ced9d27e16ab3
Alioth <67784246+zzy1127@users.noreply.github.com>
2024-10-27 23:22:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     extras_require={
         'image': ['fsspec==2024.6.1', 'ftfy==6.2.3', 'nltk==3.8', 'openai-clip==1.0.1', 'regex==2024.7.24', 'safetensors==0.4.4', 'scikit-learn==1.5.1', 'setuptools==72.1.0', 'timm==1.0.8', 'torchvision==0.19.0', 'vllm==0.6.0'],
         'video': ['einops==0.8.0', 'pandas==2.2.2', 'PyYAML==6.0.2', 'scikit-video==1.1.11', 'timm==1.0.8', 'torchvision==0.19.0'],
-        'text': ['fasttext==0.9.3', 'filelock==3.15.4', 'google-api-core==2.19.1', 'google-api-python-client==2.140.0', 'google-auth==2.33.0', 'google-auth-httplib2==0.2.0', 'googleapis-common-protos==1.63.2', 'kenlm==0.2.0', 'langkit==0.0.33', 'loguru==0.7.2', 'matplotlib==3.9.2', 'multiprocess==0.70.16', 'openai==1.44.1', 'prettytable==3.11.0', 'pyspark==3.5.2', 'sentencepiece==0.2.0', 'vendi-score==0.0.3', 'wget==3.2', 'nltk','presidio_analyzer[transformers]','presidio_anonymizer'],
+        'text': ['fasttext==0.9.3', 'filelock==3.15.4', 'google-api-core==2.19.1', 'google-api-python-client==2.140.0', 'google-auth==2.33.0', 'google-auth-httplib2==0.2.0', 'googleapis-common-protos==1.63.2', 'kenlm==0.2.0', 'langkit==0.0.33', 'loguru==0.7.2', 'matplotlib==3.9.2', 'multiprocess==0.70.16', 'openai==1.44.1', 'prettytable==3.11.0', 'pyspark==3.5.2', 'sentencepiece==0.2.0', 'vendi-score==0.0.3', 'wget==3.2', 'nltk','presidio_analyzer[transformers]','presidio_anonymizer', 'gdown'],
         'all': ['fsspec==2024.6.1', 'ftfy==6.2.3', 'nltk==3.8', 'openai-clip==1.0.1', 
             'regex==2024.7.24', 'safetensors==0.4.4', 'scikit-learn==1.5.1', 
             'setuptools==72.1.0', 'timm==1.0.8', 'torchvision==0.19.0', 'vllm==0.6.0',
@@ -19,6 +19,6 @@ setup(
             'googleapis-common-protos==1.63.2', 'kenlm==0.2.0', 'langkit==0.0.33',
             'loguru==0.7.2', 'matplotlib==3.9.2', 'multiprocess==0.70.16', 
             'openai==1.44.1', 'prettytable==3.11.0', 'pyspark==3.5.2', 
-            'sentencepiece==0.2.0', 'vendi-score==0.0.3', 'wget==3.2'],
+            'sentencepiece==0.2.0', 'vendi-score==0.0.3', 'wget==3.2', 'presidio_analyzer[transformers]','presidio_anonymizer'],
     }
 )
